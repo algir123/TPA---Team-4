26b65d023a5e20e623718709478686618f26bee9
THAUG6 <117377205+THAUG6@users.noreply.github.com>
2025-09-29 07:47:34 -0400

    Create réflexion.py
    Code pour analyser les impulsions pour la partie 3.1.2 du laboratoire CNDs

---
+++ CNDs/Réflexion.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.signal import find_peaks
+
+liste_fichier = [ _ for _ in range(1, 8)]
+
+for i in liste_fichier:
+    df = pd.read_csv("CNDs/3.1.2 rÉFLEXION/3.1.2 " + f"{i}.csv",comment='%', skip_blank_lines=True)
+    df.rename(columns={df.columns[0]: 'Time', df.columns[1]: 'Voltage'}, inplace=True) 
+    df = df.iloc[000:800]
+
+    df_out = df.iloc[0:400]
+    df_reflechi = df.iloc[400:800]
+    peaks_out, _ = find_peaks(df_out["Voltage"], height=-0.03)
+    peaks_reflechi, _ = find_peaks(df_reflechi["Voltage"], height=-0.02)
+
+    peak_times_out = df_out["Time"].iloc[peaks_out]
+    peak_times_reflechi = df_reflechi["Time"].iloc[peaks_reflechi]
+    print(f"point: {i - 0}")
+    print(peak_times_out)
+    print(peak_times_reflechi)
+
+
+    epaisseurs = []
+    for i, t_reflechi in enumerate(peak_times_reflechi.values):
+        epaisseurs.append((t_reflechi - peak_times_out.values[i]) * 6300*1000)
+    epaisseur = np.mean(epaisseurs)/2
+    std_epaisseur = np.std(epaisseurs)/2
+
+    print("Épaisseur mesurée : ", epaisseur, '±', std_epaisseur, " mm")
